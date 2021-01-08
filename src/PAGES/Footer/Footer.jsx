1566fb0e6c93344dcfdfe2ddef5683f6bc95e427
regina <reginaevadewi@gmail.com>
2021-01-08 14:20:31 +0100

    try to redeploy

--- src/PAGES/Footer/Footer.jsx
+++ src/PAGES/Footer/Footer.jsx
@@ -11,12 +11,13 @@ const Footer = () => {
                     <div className="row justify-content-center">
                         <div className="col-12 align-self-center text-center bg-light">
                             <h6 className="font-weight-bolder mt-2">CHEW & CHEER CATERING </h6>
-                            {/* <div className="mb-3">
+                            <div className="mb-3">
                                 <a href="https://www.facebook.com/indonesia.tourism/?ref=br_rs"><IconContext.Provider value={{ color: 'black', size: '30px' }}><AiFillFacebook/></IconContext.Provider></a>&nbsp;
                                 <a href="https://twitter.com/twitterid?lang=en"><IconContext.Provider value={{ color: 'black', size: '30px' }}><AiFillTwitterCircle/></IconContext.Provider></a>&nbsp;
                                 <a href="https://www.instagram.com/indtravel/?hl=en"><IconContext.Provider value={{ color: 'black', size: '30px' }}><AiFillInstagram/></IconContext.Provider></a>&nbsp;
                                 <a href="mailto:website@gmail.com?Subject=Hi%20Eva"><IconContext.Provider value={{ color: 'black', size: '30px' }}><AiFillMail/></IconContext.Provider></a>
-                            </div> */}
+                            </div>
+                            <div><p className="text-center text-secondary font-weight-bold">Copyright 2019 by Regina Eva</p></div>
                             <div><p className="text-center text-secondary font-weight-bold">Copyright 2019 by Regina Eva</p></div>
                         </div>
                     </div>

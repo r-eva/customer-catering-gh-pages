6edec7ea58609bf0d257b7d3bf57c22f06f6b3b5
reginamandasari <regina.mandasari@accenture.com>
2022-11-28 14:49:00 +0100

    change browser router to hashrouter

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import ReduxThunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware} from 'redux'
 import Reducers from './REDUX/Reducer';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker'
 
 const globalStore = createStore(Reducers, {}, applyMiddleware(ReduxThunk))
 
-ReactDOM.render(<Provider store={globalStore}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={globalStore}><HashRouter><App /></HashRouter></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

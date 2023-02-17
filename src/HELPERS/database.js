17a2a44a2185fd92abf7f200f16b7e9679687d01
reginamandasari <regina.mandasari@accenture.com>
2023-02-17 11:13:35 +0100

    move api from railway to cyclic

--- src/HELPERS/database.js
+++ src/HELPERS/database.js
@@ -1,3 +1,5 @@
-export const urlApi = "https://api-catering-gh-pages-production.up.railway.app/";
+// export const urlApi = "https://api-catering-gh-pages-production.up.railway.app/";
+export const urlApi = "https://brave-duck-jersey.cyclic.app/";
+
 
 // export const urlApi = 'http://localhost:1997/'

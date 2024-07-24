617d908d6082efd8fe436719a59038cb68d45a35
reginamandasari <regina.mandasari@accenture.com>
2024-07-24 13:12:02 +0200

    update backend web service

--- src/HELPERS/database.js
+++ src/HELPERS/database.js
@@ -1,5 +1,6 @@
 // export const urlApi = "https://api-catering-gh-pages-production.up.railway.app/";
-export const urlApi = "https://brave-duck-jersey.cyclic.app/";
-
+// export const urlApi = "https://brave-duck-jersey.cyclic.app/";
+export const urlApi =
+  'https://glacial-retreat-30704-eb66b354afde.herokuapp.com/';
 
 // export const urlApi = 'http://localhost:1997/'
